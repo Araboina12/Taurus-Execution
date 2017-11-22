ee61a722bb6141c1f1b43b467709ef67beedf583
Araboina12 <33222364+Araboina12@users.noreply.github.com>
2017-11-22 17:20:32 +0530

    Update pipeline_example.groovy

--- pipeline_example.groovy
+++ pipeline_example.groovy
@@ -5,7 +5,7 @@ node {
    stage('Performance Tests') {
     parallel(
         BlazeMeterTest: {
-            dir ('C:\Users\kumar12\Taurus Resources') {
+            dir ("C:\Users\kumar12\Taurus Resources") {
                bat 'bzt taurus_jmeter_script3.yml -report'
             }
         },
